f117ba42ce949b7a7e22dd0e3ae7a22a5d4b648d
Abdikadir Eng <abdisakariye95@gmail.com>
2025-11-06 06:59:34 -0800

    fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ const Contacts = ()=>{
         <div className="contact-us">
             <h2>Contact Us</h2>
             <div className="contacts">
-                <div className="info">
+                <div className="infoC">
                     <h3>macmiil wax-walba oo wanaagsan naga filo </h3>
                     <p>wax-walba cafimaadkaba u horeeya haddii aad danaynayso halkan waa meeshadii</p>
                     <span>waxaan ku diyaarinay cuno cafimaad  tayo  iyo dhandhanba leh</span>
